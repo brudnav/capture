3a7abd38e2e397040f6eb0d59d4962aa933852f8
brudnav <brudna.vaclav@seznam.cz>
2022-03-18 23:11:45 +0100

    4. Faq section

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,12 +1,13 @@
 import React from "react";
 import AboutSection from "../components/AboutSection";
 import ServicesSection from "../components/ServicesSection";
-
+import FaqSection from "../components/FaqSection";
 const About = () => {
   return (
     <>
       <AboutSection />
       <ServicesSection />
+      <FaqSection />
     </>
   );
 };
