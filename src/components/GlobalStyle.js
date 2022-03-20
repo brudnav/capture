ea0930d0944bbc1b218530ba6238aa53871fbeba
brudnav <brudna.vaclav@seznam.cz>
2022-03-20 09:24:26 +0100

    6. Styling Services Section

--- src/components/GlobalStyle.js
+++ src/components/GlobalStyle.js
@@ -1,10 +1,12 @@
 import { createGlobalStyle } from "styled-components";
 
 const GlobalStyle = createGlobalStyle`
+
 *{
     margin: 0;
     padding: 0;
-    box-sizing: border-box
+    box-sizing: border-box;
+    font-family: Inter, sans-serif;
 }
 body{
     background: #1b1b1b;
